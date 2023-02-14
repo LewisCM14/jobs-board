8cf910030b9c40b8a692941d31fe78c3cd21aafc
LewisCM14 <Lewiscmartin@live.co.uk>
2023-02-14 17:25:24 +0000

    allow for sorting on JobBoard

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <template>
-	<JobList :jobs="jobs" />
+	<header>
+		<div class="order">
+			<button @click="handleClick('title')">Order by title</button>
+			<button @click="handleClick('salary')">Order by salary</button>
+			<button @click="handleClick('location')">Order by location</button>
+		</div>
+	</header>
+	<JobList :jobs="jobs" :order="order" />
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
 import JobList from './components/JobList.vue';
 import Job from './types/Job';
+import OrderTerm from './types/OrderTerm';
 
 export default defineComponent({
 	name: 'App',
@@ -43,9 +51,32 @@ export default defineComponent({
 				id: '5',
 			},
 		]);
-		return { jobs };
+		const order = ref<OrderTerm>('title');
+
+		const handleClick = (term: OrderTerm) => {
+			order.value = term;
+		};
+
+		return { jobs, handleClick, order };
 	},
 });
 </script>
 
-<style></style>
+<style>
+header {
+	text-align: center;
+}
+header .order {
+	margin-top: 20px;
+}
+button {
+	margin: 0 10px;
+	color: #1195c9;
+	border: 3px solid #1195c9;
+	background: #d5f0ff;
+	padding: 8px 16px;
+	border-radius: 4px;
+	cursor: pointer;
+	font-weight: bold;
+}
+</style>
--- src/components/JobList.vue
+++ src/components/JobList.vue
@@ -1,7 +1,8 @@
 <template>
 	<div class="job-list">
+		<p>Ordered by {{ order }}</p>
 		<ul>
-			<li v-for="job in jobs" :key="job.id">
+			<li v-for="job in orderedJobs" :key="job.id">
 				<h2>{{ job.title }} in {{ job.location }}</h2>
 				<div class="salary">
 					<p>{{ job.salary }} rupees</p>
@@ -22,7 +23,8 @@
 
 <script lang="ts">
 import Job from '@/types/Job';
-import { PropType, defineComponent } from 'vue';
+import OrderTerm from '@/types/OrderTerm';
+import { PropType, defineComponent, computed } from 'vue';
 
 export default defineComponent({
 	props: {
@@ -30,6 +32,18 @@ export default defineComponent({
 			required: true,
 			type: Array as PropType<Job[]>,
 		},
+		order: {
+			required: true,
+			type: String as PropType<OrderTerm>,
+		},
+	},
+	setup(props) {
+		const orderedJobs = computed(() => {
+			return [...props.jobs].sort((a: Job, b: Job) => {
+				return a[props.order] > b[props.order] ? 1 : -1;
+			});
+		});
+		return { orderedJobs };
 	},
 });
 </script>
